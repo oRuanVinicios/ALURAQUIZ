e043d53b7e77818eee8dfa1f2e0caa596dedec7a
Ruan Almeida <oruanvinicios@gmail.com>
2026-01-04 13:29:55 -0300

    iniciando a pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main>
+      <header>
+        <h1>Teste suas habilidades</h1>
+      </header>
+      <p>
+        Teste os seus conhecimentos sobre o universo marvel e divirta-se criando o seu AluraQuiz
+      </p>
+
+      <p>
+        formulario/botao
+      </p>
+      <footer>
+        <p>
+          Orgulhosamente criado com o DevSoutinho e Alura
+        </p>
+      </footer>
+    </main>
+  )
 }

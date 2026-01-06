8b886039bfc52b31f4110f548cecfc894637a871
Ruan Almeida <oruanvinicios@gmail.com>
2026-01-05 22:00:15 -0300

    Foi necessario o uso de classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
+import cardStyles from './card.module.css';
 export default function page() {
   return (
     <main>
-      <header>
-        <h1>Teste suas habilidades</h1>
-      </header>
-      <p>
-        Teste os seus conhecimentos sobre o universo marvel e divirta-se criando o seu AluraQuiz
-      </p>
-
-      <p>
-        formulario/botao
-      </p>
-      <footer>
+      <div className={cardStyles.card}>
+        <header>
+          <h1 className="title">Teste suas habilidades</h1>
+        </header>
         <p>
-          Orgulhosamente criado com o DevSoutinho e Alura
+          Teste os seus conhecimentos sobre o universo marvel e divirta-se criando o seu AluraQuiz
         </p>
-      </footer>
+        <p>
+          formulario/botao
+        </p>
+        <footer>
+          <p>
+            Orgulhosamente criado com o DevSoutinho e Alura
+          </p>
+        </footer>
+      </div>
     </main>
   )
 }

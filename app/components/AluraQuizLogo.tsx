b27548c8e4c7e49f589b710562deba94d676437d
Ruan Almeida <oruanvinicios@gmail.com>
2026-02-09 20:12:25 -0300

    Pasta components

---
+++ app/components/AluraQuizLogo.tsx
@@ -0,0 +1,23 @@
+export function AluraQuizLogo() {
+    return (<svg width="135" height="67" viewBox="0 0 135 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g clip-path="url(#clip0_19035_367)">
+    <mask id="path-1-outside-1_19035_367" maskUnits="userSpaceOnUse" x="2" y="2" width="110" height="50" fill="black">
+      <rect fill="white" x="2" y="2" width="110" height="50"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M32.0345 4H36.8354C37.0766 4 37.2787 4.186 37.2787 4.4158V50H32.0345V4Z"/>
+    </mask>
+    
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.0345 4H36.8354C37.0766 4 37.2787 4.186 37.2787 4.4158V50H32.0345V4Z" fill="currentColor"/>
+    <path d="M86.8469 62.368C84.8917 63.0142 83.211 62.938 81.805 62.1393C80.3989 61.3406 79.3095 59.9506 78.5368 57.9693" stroke="currentColor" stroke-width="2"/>
+    <path d="M105.647 55.5391C105.682 55.8179 105.602 56.0407 105.407 56.2075C105.219 56.3894 104.935 56.5424 104.555 56.6664" stroke="currentColor" stroke-width="2"/>
+    <path d="M105.645 40.4037C105.462 39.9349 105.456 39.4674 105.627 39.0012C105.814 38.5296 106.195 38.1983 106.77 38.0073" stroke="currentColor" stroke-width="2"/>
+    <path d="M122.195 49.9925C118.16 51.8214 115.862 52.8715 115.3 53.1426C114.715 53.404 114.415 53.5375 114.4 53.5434" stroke="currentColor" stroke-width="2"/>
+    <path d="M127.144 43.2346C127.205 43.6755 127.15 44.0096 126.979 44.2369C126.824 44.4587 126.476 44.6593 125.935 44.8387" stroke="currentColor" stroke-width="2"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M86.918 48.6425C86.2653 48.1582 85.4789 48.0681 84.5588 48.3721" fill="currentColor"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_19035_367">
+      <rect width="134.373" height="67" fill="white"/>
+    </clipPath>
+  </defs>
+</svg>);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
 import cardStyles from './card.module.css';
 import homeStyles from './home.module.css';
+import { AluraQuizLogo } from './components/AluraQuizLogo';
 export default function page() {
   return (
-    <main className= {homeStyles.homeScreen} style={{flex: 1}}>
+    <main className={homeStyles.homeScreen} style={{ flex: 1 }}>
+      <div>
+        <AluraQuizLogo />
+      </div>
+
       <div className={cardStyles.card}>
-      <header className={cardStyles.cardHeader}>
-        <h1 className={cardStyles.cardHeaderTitle}>Teste suas habilidades</h1>
-      </header>
-      <p>
-        Teste os seus conhecimentos sobre o universo marvel e divirta-se criando o seu AluraQuiz
-      </p>
-      <p>
-        formulario/botao
-      </p>
+        <header className={cardStyles.cardHeader}>
+          <h1 className={cardStyles.cardHeaderTitle}>Teste suas habilidades</h1>
+        </header>
+        <section className={cardStyles.cardBody}>
+          <p>
+            Teste os seus conhecimentos sobre o universo marvel e divirta-se criando o seu AluraQuiz
+          </p>
+          <p>
+            formulario/botao
+          </p>
+        </section>
+      </div>
       <footer>
         <p>
           Orgulhosamente criado com o DevSoutinho e Alura
         </p>
-      </footer> 
-      </div>
+      </footer>
     </main>
   )
 }

c98b325e382abcc458a0cf84918ba6ccc9d7f5e7
Ruan Almeida <oruanvinicios@gmail.com>
2026-02-17 20:00:46 -0300

    SVG do footer estava com propriedades de texto que eu preferi separar e aplicar estilo com o CSS

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -6,7 +6,11 @@ import FooterStyles from './footer.module.css'
 export function Footer() {
     return (
         <footer className={FooterStyles.footer}>
-            <AluraLogo />     
+            <AluraLogo />  
+            <p>
+                Orgulhosamente criado com o <br /> DevSoutinho e Alura
+            </p>    
+
         </footer>
     )
 }
